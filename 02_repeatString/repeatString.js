ad4150f872be6925ad9f35ea95a0b1e847b7221b
KevinJN03 <114575604+KevinJN03@users.noreply.github.com>
2023-05-24 20:32:59 +0100

    Completed exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    finalWord = "";
+    let i = 0;
 
+    if(num >= 0){
+    while(i < num){
+        
+        finalWord += word
+        i++
+    }} else {
+        finalWord = "ERROR"
+    }
+    return finalWord
 };
 
 // Do not edit below this line
